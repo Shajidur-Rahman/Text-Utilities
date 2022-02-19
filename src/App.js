3caa0cae7a195c8b315cf59d2de56c1d869b6663
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2022-02-19 11:26:28 +0600

    cwh

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import Navbar from "./components/Navbar"
 import TextForm from "./components/TextForm"
+import About from "./components/About"
+
 
 
 function App() {  
   return (
     <>
       <Navbar title="Textutils"/>
-
-      <div className="container my-5">
+      {/* <div className="container my-5">
       <TextForm heading="Enter your Text to analyze"/>
-      </div>
-      
+      </div> */}
+
+      <About/>
+
     </>
   );
 }
