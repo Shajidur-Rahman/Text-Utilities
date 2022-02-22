8000d100f9c1f1aaed0944035d4d88e232de0ea8
Shajidur-Rahman <shajidurrahmansaad10@gmail.com>
2022-02-22 21:53:19 +0600

    route finished

--- src/App.js
+++ src/App.js
@@ -2,18 +2,24 @@ import './App.css';
 import Navbar from "./components/Navbar"
 import TextForm from "./components/TextForm"
 import About from "./components/About"
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 
-function App() {  
+function App() {
   const [mode, setMode] = useState("light")
 
-  const toggleMode = ()=>{
-    if(mode === "light"){
+  const toggleMode = () => {
+    if (mode === "light") {
       setMode("dark")
       document.body.style.backgroundColor = "#343a40"
       document.body.style.color = "white"
-    } else{
+    } else {
       setMode("light")
       document.body.style.backgroundColor = "white"
       document.body.style.color = "black"
@@ -22,12 +28,40 @@ function App() {
 
   return (
     <>
-      <Navbar title="Textutils" mode={mode} toggleMode={toggleMode}/>
-      <div className="container my-5">
-      <TextForm heading="Enter your Text to analyze" mode={mode}/>
-      </div>
 
-      <About mode={mode} toggleMode={toggleMode}/>
+
+
+      <Router>
+        <Routes>
+          {/* <Route path="/">
+            <Navbar title="Textutils" mode={mode} toggleMode={toggleMode} />
+          </Route> */}
+
+
+          <Route exact path="/" element={
+
+            <>
+              <Navbar title="Textutils" mode={mode} toggleMode={toggleMode} />
+
+              <div className="container my-5">
+                <TextForm heading="Enter your Text to analyze" mode={mode} />
+              </div>
+
+            </>
+
+
+
+          } />
+          <Route path='/about' element={
+            <>
+              <Navbar title="Textutils" mode={mode} toggleMode={toggleMode} />
+              <About/>
+            </>} />
+        </Routes>
+      </Router>
+
+
+      {/* <About/> */}
 
     </>
   );
